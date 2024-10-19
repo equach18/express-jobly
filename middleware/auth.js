e4ab549943ce1302867b886b5bdc0646c5e00d6f
Elaine Quach <equach18@gmail.com>
2024-10-18 19:11:22 -0500

    updated middleware to authorize admin status and also admin or correct users. Tests completed for the two methods added.

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,7 +6,6 @@ const jwt = require("jsonwebtoken");
 const { SECRET_KEY } = require("../config");
 const { UnauthorizedError } = require("../expressError");
 
-
 /** Middleware: Authenticate user.
  *
  * If a token was provided, verify it, and, if valid, store the token payload
@@ -42,8 +41,39 @@ function ensureLoggedIn(req, res, next) {
   }
 }
 
+/** Middleware to use when the user must be an admin.
+ *
+ * If not, raises Unauthorized.
+ */
+function ensureAdmin(req, res, next) {
+  try {
+    if (!res.locals.user || !res.locals.user.isAdmin)
+      throw new UnauthorizedError("You must be an admin.");
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+}
+
+/** Middleware to use when the user must be that user or an admin. 
+ * 
+ * If not, raise unauthorized error 
+*/
+function ensureCorrectUserOrAdmin(req, res, next) {
+  try {
+    const user = res.locals.user;
+    if (!(user && (user.isAdmin || user.username === req.params.username))) {
+      throw new UnauthorizedError();
+    }
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+}
 
 module.exports = {
   authenticateJWT,
   ensureLoggedIn,
+  ensureAdmin,
+  ensureCorrectUserOrAdmin,
 };
